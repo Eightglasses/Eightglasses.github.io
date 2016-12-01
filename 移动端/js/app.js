34b4f028679f6066b4ee4ce200f6e122d9e6387a
Eightglasses <357694670@qq.com>
2016-12-01 16:41:29 +0800

    Update app.js

--- 移动端/js/app.js
+++ 移动端/js/app.js
@@ -16,7 +16,7 @@ var arrimg = [{
 	src: 'img/5.jpg',
 	title: '一只孤舟'
 }];
-
+var tag = ['服务好', '景色赞', '千篇一律', '看人海', '挤爆了', '服务好', '态度好', '景色赞', '服务好', ]
 var welcome = document.querySelector('.welcome');
 var oTab = document.querySelector('.tab');
 var tabChild = oTab.children[0].children;
@@ -32,7 +32,8 @@ window.addEventListener('deviceorientation', function(e) {
 		welcome.style.transform = 'translateX(' + y + 'px)'
 	}
 });
-//滚动图
+//滚动图<section class="showpage"> 数据arrimg
+
 $(arrimg).each(function(i) {
 	$('<li></li>').append($('<img>').attr('src', arrimg[i].src)).appendTo($('.tab ul'));
 	$('.picMask nav').append($('<span>'));
@@ -65,12 +66,20 @@ function star(index) {
 		oScorea[i].index = i;
 		oScorea[i].onclick = function() {
 			for(var i = 0; i < oScorea.length; i++) {
-				if(i<=this.index){
+				if(i <= this.index) {
 					oScorea[i].style.backgroundPositionX = 0;
-				}else{
+				} else {
 					oScorea[i].style.backgroundPositionX = '-1rem';
 				}
 			}
 		}
 	}
 }
+
+//添加标签<seciton class= 'tag'> ;数据tag
+$(tag).each(function(index) {
+	$('<li>').append($('<input>').attr('type', 'radio')).append($('<span>').html(tag[index])).appendTo($('.tag ul'))
+});
+$('.tag li').click(function(){
+		$(this).addClass('active').siblings().removeClass('active');
+	})

9d78358b5c0d964ec360c3f6d1e5328209b6c4e2
Eightglasses <357694670@qq.com>
2016-12-08 16:57:31 +0800

    Update data.js

--- 桌面/js/data.js
+++ 桌面/js/data.js
@@ -1,17 +1,22 @@
-var data = [{
+var dataList = [{
 	name: '百度云',
 	href: '../百度云/index.html',
-	id:1
+	pid: 1,
+	id: 1
 }, {
 	name: '小游戏',
 	href: '',
-	id:1
+	pid: 1,
+	id: 2
+
 }, {
 	name: '移动端展示',
 	href: '',
-	id:1
+	pid: 1,
+	id: 3
 }, {
 	name: 'todolist',
 	href: '',
-	id:1
+	pid: 1,
+	id: 4
 }]

a00172495f7f8768bc92390c3b91e02c0d924a23
Eightglasses <357694670@qq.com>
2016-12-14 16:15:12 +0800

    Update desktop.js

--- 桌面/js/desktop.js
+++ 桌面/js/desktop.js
@@ -1,154 +1,125 @@
-window.onload = function() {
-	//	get(); //拿本地存储数据
-	var oMain = document.getElementById('main');
-	var oDesktop = document.getElementById('desktop');
-	var oBottom = document.getElementById('bottom');
-	var divList = document.getElementsByClassName('divlist');
-	var oRemove = document.getElementsByClassName('removeItem')[0];
-	var oNew_file = document.getElementsByClassName('new_file')[0];
-	var oContextmenu1 = document.getElementById('contextmenu1');
-	var oContextmenu2 = document.getElementById('contextmenu2');
-	var menu2child = oContextmenu2.children[0].children;
-	var menu1child = oContextmenu1.children;
-	var oInvisible = document.getElementById('invisible'); //弹出框
-	var oBody = document.body;
-	var data = null; //本地缓存数据
+var oMain = document.getElementById('main');
+var oDesktop = document.getElementById('desktop');
+var oBottom = document.getElementById('bottom');
+var divList = document.getElementsByClassName('divlist');
+var oRemove = document.getElementsByClassName('removeItem')[0];
+var oNew_file = document.getElementsByClassName('new_file')[0];
+var oContextmenu1 = document.getElementById('contextmenu1');
+var oContextmenu2 = document.getElementById('contextmenu2');
+var menu2child = oContextmenu2.children[0].children;
+var menu1child = oContextmenu1.children;
+var oInvisible = document.getElementById('invisible'); //弹出框
+var oBody = document.body;
+var data = null; //本地缓存数据
 
-	//获取本地缓存
-	var getItem = localStorage.getItem('str');
-	data = JSON.parse(getItem);
-	if(data == null) { //如果有本地缓存用本地缓存没有本地缓存用datalist的数据
-		data = dataList;
-	} else {
-		data = data;
-	}
-	//默认加载
-	windowR();
-	//数据
-	save();
-	//存数据
-	function save() {
-		var str = JSON.stringify(data);
-		localStorage.setItem('str', str);
-	}
+getdata();
 
-	function windowR() {
-		//加载需要显示的内容
-		var oMainc = oMain.children;
-		if(oMainc.length == '0') { //判断omain是否有子集，有的话就不添加了
-			$(data).each(function(index) {
-				$('#main').append($('<div>').addClass('divlist').append($('<a>').append($('<span>').addClass('divlistimg')).append($('<p>').html(data[index].name))));
-			});
-		};
+//默认加载
+windowR();
+//数据
+save();
+//存数据
 
-		location();
+//添加div
+//	function windowR() {
+//		//加载需要显示的内容
+//		var oMainc = oMain.children;
+//		//			$(data).each(function(index) {
+//		//				if(data[index].pid == 1) {
+//		//					$('#main').append($('<div>').addClass('divlist').append($('<a>').append($('<span>').addClass('divlistimg')).append($('<p>').html(data[index].name))));
+//		//				}
+//		//			});
+//		var str1 = '';
+//		for(var i = 0; i < data.length; i++) {
+//			if(data[i].pid == 1) {
+//				str1 += '<div class="divlist"><a><span class="divlistimg"></span><p>' + data[i].name + '</p></a></div>'
+//			}
+//		}
+//		oMain.innerHTML = str1;
+//
+//		location();
+//
+//	}
 
-	}
-
-	function location() { //宽高自适用
-		var elem = document.documentElement;
-		var elemWidth = elem.clientWidth;
-		var elemHeight = elem.clientHeight;
-		oBody.style.width = elemWidth + 'px';
-		oBody.style.height = elemHeight + 'px';
-		oMain.style.height = elemHeight + 'px';
-		oMain.style.backgroundSize = '100% 100%';
-		//图标位置确定 ，lnum一列能放几个图标
-		var lNum = Math.floor(elemHeight / divList[0].offsetHeight);
-		oMain.lNum = Math.floor(elemHeight / divList[0].offsetHeight);
-		for(var i = 0; i < divList.length; i++) {
-			one.move(divList[i]); //移动
-			one.contextmenu(divList[i]); //右键
-			one.dbl(divList[i], 'data[i].href', oInvisible); //双击
-			var oleft = Math.floor(i / lNum) * divList[i].offsetWidth;
-			var otop = (i % lNum) * 130;
-			$('.divlist').eq(i).animate({
-				left: oleft,
-				top: otop
-			}, 100);
-			oMain.i = i;
-		}
-	}
-	var child = oInvisible.children[0].children[1].children;
-	child[2].onclick = function(e) {
-		e.cancelBubble = true;
-
-		oInvisible.style.display = 'none';
-	}
-
-	child[1].onoff = true;
-	child[1].onclick = function(e) { //iframe窗口放大缩小
-			e.cancelBubble = true;
-			if(child[1].onoff) {
-				oInvisible.style.width = '100%';
-				oInvisible.style.height = '100%';
-				this.style.backgroundPosition = 'left -60px'
-				oinposition(oInvisible, '28', '0');
-				child[1].onoff = false;
-			} else {
-				oInvisible.style.width = '750px';
-				oInvisible.style.height = '450px';
-				child[1].onoff = true;
-				oinposition(oInvisible, '28', '30');
-			}
-
-		}
-		//窗口大小改变时重新加载
-	window.onresize = function() {
-		windowR();
-	};
-
-	document.onmousedown = function() {
-		contextmenu2.style.display = 'none';
-		contextmenu1.style.display = 'none';
-		return false;
-	}
-	document.oncontextmenu = function(e) {
-		var x = e.clientX;
-		var y = e.clientY;
-		contextmenu2.style.left = x + 'px';
-		contextmenu2.style.top = y + 'px';
-		contextmenu2.style.display = 'block';
-		return false;
-	};
-	menu2child[1].onmousedown = function(e) { //刷新页面
-		e.cancelBubble;
-		document.location.reload();
-	};
-	menu2child[0].onmousedown = function(e) { //整理桌面
-		e.cancelBubble;
-		windowR();
-	};
-	oRemove.onmousedown = function() {
-		localStorage.removeItem('str');
-		document.location.reload();
-	}
-
-	oNew_file.onmousedown = function(e) { //新建文件夹
-		oContextmenu2.style.display = 'none';
-		var newdata = '';
-		//新数据
-		newdata = {
-			name: '新建文件夹',
-			pid: 1,
-			id: data.length + 1,
-			href: ''
-		};
-		data.push(newdata);
-		save();
-		$('#main').append($('<div>').addClass('divlist').append($('<a>').append($('<span>').addClass('divlistimg')).append($('<p>').html('新建文件夹'))));
-		var ol = Math.floor((oMain.i + 1) / oMain.lNum) * divList[oMain.i + 1].offsetWidth;
-		var ot = ((oMain.i + 1) % oMain.lNum) * 130;
-		$('#main div').each(function(index) {
-			$('#main div').last().animate({
-				left: ol,
-				top: ot
-			})
-		})
-
-		oMain.i++;
-		location();
-		e.cancelBubble = true;
-	};
-
+var child = oInvisible.children[0].children[1].children;
+child[2].onclick = function(e) {
+	e.cancelBubble = true;
+	oInvisible.style.display = 'none';
 }
+
+child[1].onoff = true;
+child[1].onclick = function(e) { //iframe窗口放大缩小
+		e.cancelBubble = true;
+		if(child[1].onoff) {
+			oInvisible.style.width = '100%';
+			oInvisible.style.height = '100%';
+			this.style.backgroundPosition = 'left -60px'
+			oinposition(oInvisible, '28', '0');
+			child[1].onoff = false;
+		} else {
+			oInvisible.style.width = '750px';
+			oInvisible.style.height = '450px';
+			child[1].onoff = true;
+			oinposition(oInvisible, '28', '30');
+		}
+
+	}
+	//窗口大小改变时重新加载
+window.onresize = function() {
+	windowR();
+};
+
+document.onmousedown = function() {
+	contextmenu2.style.display = 'none';
+	contextmenu1.style.display = 'none';
+	return false;
+}
+document.oncontextmenu = function(e) {
+	var x = e.clientX;
+	var y = e.clientY;
+	contextmenu2.style.left = x + 'px';
+	contextmenu2.style.top = y + 'px';
+	contextmenu2.style.display = 'block';
+	return false;
+};
+menu2child[1].onmousedown = function(e) { //刷新页面
+	e.cancelBubble;
+	document.location.reload();
+};
+menu2child[0].onmousedown = function(e) { //整理桌面
+	e.cancelBubble;
+	windowR();
+};
+oRemove.onmousedown = function() {
+	localStorage.removeItem('str');
+	document.location.reload();
+};
+
+oNew_file.onmousedown = function(e) { //新建文件夹
+	oContextmenu2.style.display = 'none';
+	var newdata = '';
+	//新数据
+	newdata = {
+		name: '新建文件夹',
+		pid: 1,
+		id: data.length ,
+		href: '',
+		cid: 1
+	};
+	data.push(newdata);
+	save();
+	
+	$('#main').append($('<div>').attr('zid',newdata.id).addClass('divlist').append($('<a>').append($('<span>').addClass('divlistimg')).append($('<p>').html('新建文件夹'))));
+	var ol = Math.floor((oMain.i + 1) / oMain.lNum) * divList[oMain.i + 1].offsetWidth;
+	var ot = ((oMain.i + 1) % oMain.lNum) * 130;
+	$('#main div').each(function(index) {
+		$('#main div').last().animate({
+			left: ol,
+			top: ot
+		})
+	})
+
+	oMain.i++;
+	location();
+	e.cancelBubble = true;
+};

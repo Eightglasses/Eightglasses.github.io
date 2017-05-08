3a424e57391a13d91c018e24cf5b01f06b8f719b
songzhiyu <357694670@qq.com>
2017-05-08 10:20:37 +0800

    ele

---
+++ eleme/src/components/navbar.vue
@@ -0,0 +1,67 @@
+<template>
+	<!-- 我只是个头 -->
+	<div class="back_box">
+		<div class="back_arrow" @click="back_one">
+			< 
+		</div>
+		<h3>{{title}}</h3>
+	</div>
+
+</template>
+
+<script>
+	export default {
+		name: 'back_bar',
+		props: ['title'],
+		data() {
+			return {
+
+			};
+		},
+		mounted() {
+
+		},
+		computed: {
+
+		},
+		methods: {
+			back_one() {
+				this.$router.go(-1);
+			}
+		}
+	};
+</script>
+
+<style>
+	.back_box {
+		width: 16rem;
+		height: 2rem;
+		line-height: 2rem;
+		position: fixed;
+		background: #0096ff;
+		top: 0;
+		left: 0;
+		right: 0;
+		z-index: 999;
+	}
+	
+	h3 {
+		color: #fff;
+		width: 100%;
+		display: block;
+		font-size: .52rem;
+		text-align: center;
+		font-weight: 700;
+	}
+	
+	div.back_arrow {
+		width: 2rem;
+		height: 2rem;
+		position: absolute;
+		left: 0;
+		top: 0;
+		line-height: 2rem;
+		color: #fff;
+		font-size: 1.5rem;
+	}
+</style>

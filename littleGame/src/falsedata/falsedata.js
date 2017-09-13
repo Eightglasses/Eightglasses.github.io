19da51653b19330c678b7cb8712b3975b3ef5b25
songzy <357694670@qq.com>
2017-09-13 18:05:40 +0800

    '测试一下vscode的git命令台提交好用不好用'

--- littleGame/src/falsedata/falsedata.js
+++ littleGame/src/falsedata/falsedata.js
@@ -1,17 +1,23 @@
-var account = {
-	admin: {
-		passwords: '000000',
-		act: '2',
-		health: '100'
-	},
-	admin1: {
-		passwords: '000000',
-		act: '3',
-		health: '100'
-	},
-	admin2: {
-		passwords: '0',
-		act: '999',
-		health: '10000'
-	}
-}
+// admin帐号
+//passwords密码
+//attack攻击
+//health血量
+var account = [{
+        lid: admin,
+        passwords: '000000',
+        attack: '2',
+        health: '100'
+    },
+    {
+        lid: admin1,
+        passwords: '000000',
+        attack: '3',
+        health: '100'
+    },
+    {
+        lid: admin2,
+        passwords: '0',
+        attack: '999',
+        health: '10000'
+    }
+]

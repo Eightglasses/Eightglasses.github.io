4d28475ab27049d4834e1be1cabaa722d485bfb1
szy <szy@xibei.com>
2021-04-30 22:02:53 +0800

    pad

---
+++ pad/js/dish.js
@@ -0,0 +1,474 @@
+var dish = {
+    "classify": [
+        {
+            "id": "22",
+            "name": "蒙古牛大骨"
+        },
+        {
+            "id": "7",
+            "name": "酒水饮料"
+        }
+    ],
+    "page": {
+        "22": [
+            {
+                "ad_id": 575,
+                "ad_name": "pad视频",
+                "ad_img": "",
+                "use_source_name": "357c5a4c02b74d0030a8a0b006d50b09.mp4",
+                "types": "video",
+                "block": "ad"
+            },
+            {
+                "id": 1020033,
+                "store_id": 200138,
+                "class_id": 1,
+                "name": "美极木耳",
+                "name_en": " Black Fungus with Maggi Seasoning ",
+                "code": "10009",
+                "price": "23.00",
+                "member_price": "23.00",
+                "room_price": "23.00",
+                "room_member_price": "23.00",
+                "image_4": "http://static.xibeidev.com/uploads/food/0977e438658fadf21034cbe8111435c3.png",
+                "image_5": "http://static.xibeidev.com/uploads/food/2f6d9043a4e6361d44987e0bb2306ea8.jpg",
+                "image_6": "http://static.xibeidev.com/uploads/food/14db235574de012af6d68db1645509e0.png",
+                "image_7": "http://static.xibeidev.com/uploads/food/7888a22748cc3645088f54d8ff142473.png",
+                "unit": "份",
+                "unit2": "180g",
+                "attrs": [
+                    {
+                        "id": "1",
+                        "name": "辣度",
+                        "values": [
+                            {
+                                "id": "1",
+                                "name": "不辣"
+                            },
+                            {
+                                "id": "2",
+                                "name": "微辣"
+                            },
+                            {
+                                "id": "3",
+                                "name": "中辣"
+                            }
+                        ],
+                        "multiple": 0
+                    },
+                    {
+                        "id": "2",
+                        "name": "忌口",
+                        "values": [
+                            {
+                                "id": "5",
+                                "name": "免葱"
+                            },
+                            {
+                                "id": "7",
+                                "name": "免蒜"
+                            },
+                            {
+                                "id": "8",
+                                "name": "免香菜"
+                            },
+                            {
+                                "id": "6",
+                                "name": "免姜"
+                            }
+                        ],
+                        "multiple": 1
+                    }
+                ],
+                "is_new": 0,
+                "is_specialty": 0,
+                "is_essential": 0,
+                "is_recommend": 0,
+                "is_child": 0,
+                "is_weigh": 0,
+                "spicy_level": 1,
+                "is_must_spicy": 1,
+                "description": "甄选东北秋木耳，肉质肥厚，口感脆爽！主料：木耳不可免辣过敏原食物：芥末",
+                "weigh_description": "",
+                "state": 1,
+                "updatetime": 1619093569,
+                "dish_type": 1,
+                "dish_modifiers": "",
+                "dish_specs": "",
+                "sale_num": 1,
+                "image_type": "10",
+                "use_source_name": "0977e438658fadf21034cbe8111435c3.png",
+                "use_source_name2": "0977e438658fadf21034cbe8111435c3.png",
+                "block": "dish"
+            },
+            {
+                "id": 1020187,
+                "store_id": 200138,
+                "class_id": 1,
+                "name": "果蔬大拌菜",
+                "name_en": "guoshudabancai",
+                "code": "10058",
+                "price": "39.00",
+                "member_price": "39.00",
+                "room_price": "39.00",
+                "room_member_price": "39.00",
+                "image_4": "http://static.xibeidev.com/uploads/food/5e932e650be3f7008d8cc9416e83fcfb.png",
+                "image_5": "http://static.xibeidev.com/uploads/food/79d5811ef61fd55cc05e36826b7e2925.png",
+                "image_6": "http://static.xibeidev.com/uploads/food/53dbc1d78faafc1be69a407d40336bfc.png",
+                "image_7": "http://static.xibeidev.com/uploads/food/cffb353d97539765e6260deb341593e2.png",
+                "unit": "份",
+                "unit2": "220g",
+                "attrs": [],
+                "is_new": 0,
+                "is_specialty": 0,
+                "is_essential": 0,
+                "is_recommend": 0,
+                "is_child": 1,
+                "is_weigh": 0,
+                "spicy_level": 3,
+                "is_must_spicy": 0,
+                "description": "update app_version set app_version =2.0.2",
+                "weigh_description": "update app_version set app_version =2.0.2",
+                "state": 1,
+                "updatetime": 1619093569,
+                "dish_type": 1,
+                "dish_modifiers": "",
+                "dish_specs": "",
+                "sale_num": 1,
+                "image_type": "10",
+                "use_source_name": "0977e438658fadf21034cbe8111435c3.png",
+                "use_source_name2": "0977e438658fadf21034cbe8111435c3.png",
+                "block": "dish"
+            },
+            {
+                "id": 1020186,
+                "store_id": 200138,
+                "class_id": 1,
+                "name": "手撕椒麻鸡",
+                "name_en": "Shredded Chicken with Chili and Peppers",
+                "code": "10010",
+                "price": "56.00",
+                "member_price": "56.00",
+                "room_price": "56.00",
+                "room_member_price": "56.00",
+                "image_4": "http://static.xibeidev.com/uploads/food/131b22eac9afca835c699a618e39e945.png",
+                "image_5": "http://static.xibeidev.com/uploads/food/89c59f73f7411753b92d2a31d976ec7d.png",
+                "image_6": "http://static.xibeidev.com/uploads/food/720ba67d0a10ec4c717cea57224691ef.png",
+                "image_7": "http://static.xibeidev.com/uploads/food/9c61c9cc28198eaf8964024a16f87866.png",
+                "unit": "份",
+                "unit2": "260g",
+                "attrs": [
+                    {
+                        "id": "1",
+                        "name": "辣度",
+                        "values": [
+                            {
+                                "id": "1",
+                                "name": "不辣"
+                            },
+                            {
+                                "id": "2",
+                                "name": "微辣"
+                            },
+                            {
+                                "id": "3",
+                                "name": "中辣"
+                            }
+                        ],
+                        "multiple": 0
+                    },
+                    {
+                        "id": "2",
+                        "name": "忌口",
+                        "values": [
+                            {
+                                "id": "5",
+                                "name": "免葱"
+                            },
+                            {
+                                "id": "7",
+                                "name": "免蒜"
+                            },
+                            {
+                                "id": "8",
+                                "name": "免香菜"
+                            },
+                            {
+                                "id": "6",
+                                "name": "免姜"
+                            }
+                        ],
+                        "multiple": 1
+                    }
+                ],
+                "is_new": 0,
+                "is_specialty": 0,
+                "is_essential": 0,
+                "is_recommend": 0,
+                "is_child": 0,
+                "is_weigh": 0,
+                "spicy_level": 1,
+                "is_must_spicy": 1,
+                "description": "麻醇咸鲜，越嚼越香，质地软嫩，微麻微辣主要原料：鸡肉",
+                "weigh_description": "",
+                "state": 1,
+                "updatetime": 1619093569,
+                "dish_type": 1,
+                "dish_modifiers": "",
+                "dish_specs": "",
+                "sale_num": 1,
+                "image_type": "9",
+                "use_source_name": "0977e438658fadf21034cbe8111435c3.png",
+                "use_source_name2": "0977e438658fadf21034cbe8111435c3.png",
+                "block": "dish"
+            },
+
+        ],
+        "7": [
+            {
+                "id": 1020220,
+                "store_id": 200138,
+                "class_id": 7,
+                "name": "可口可乐330ML",
+                "name_en": "",
+                "code": "60361",
+                "price": "8.00",
+                "member_price": "8.00",
+                "room_price": "8.00",
+                "room_member_price": "8.00",
+                "image_4": "http://static.xibeidev.com/uploads/food/d89258429cb5b57d3d4508b793c059e6.png",
+                "image_5": "http://static.xibeidev.com/uploads/food/f2e3e349c95a6acc088d72af5576712d.png",
+                "image_6": "http://static.xibeidev.com/uploads/food/065525dd90df11386f9a5309db4c53e8.jpg",
+                "image_7": "http://static.xibeidev.com/uploads/food/5006e624ca1642ae73589744f2dc533a.jpg",
+                "unit": "听",
+                "unit2": "330ml",
+                "attrs": [],
+                "is_new": 0,
+                "is_specialty": 0,
+                "is_essential": 0,
+                "is_recommend": 0,
+                "is_child": 0,
+                "is_weigh": 0,
+                "spicy_level": 0,
+                "is_must_spicy": 0,
+                "description": "",
+                "weigh_description": "",
+                "state": 1,
+                "updatetime": 1619093569,
+                "dish_type": 1,
+                "dish_modifiers": "",
+                "dish_specs": "",
+                "sale_num": 1,
+                "image_type": "9",
+                "use_source_name": "0977e438658fadf21034cbe8111435c3.png",
+                "use_source_name2": "0977e438658fadf21034cbe8111435c3.png",
+                "block": "dish"
+            },
+            {
+                "id": 1020048,
+                "store_id": 200138,
+                "class_id": 7,
+                "name": "昆仑山350ML",
+                "name_en": "",
+                "code": "60362",
+                "price": "10.00",
+                "member_price": "10.00",
+                "room_price": "10.00",
+                "room_member_price": "10.00",
+                "image_4": "http://static.xibeidev.com/uploads/food/1287fd39e289be59c16d979b06aab479.png",
+                "image_5": "http://static.xibeidev.com/uploads/food/f8afa02601218c0d80ec72416bd174a6.png",
+                "image_6": "http://static.xibeidev.com/uploads/food/9a751ae744d3cbcc0563e637ee3edad1.png",
+                "image_7": "",
+                "unit": "瓶",
+                "unit2": "350ml",
+                "attrs": [],
+                "is_new": 0,
+                "is_specialty": 0,
+                "is_essential": 0,
+                "is_recommend": 0,
+                "is_child": 0,
+                "is_weigh": 0,
+                "spicy_level": 0,
+                "is_must_spicy": 0,
+                "description": "",
+                "weigh_description": "",
+                "state": 1,
+                "updatetime": 1619093569,
+                "dish_type": 1,
+                "dish_modifiers": "",
+                "dish_specs": "",
+                "sale_num": 1,
+                "image_type": "8",
+                "use_source_name": "0977e438658fadf21034cbe8111435c3.png",
+                "use_source_name2": "0977e438658fadf21034cbe8111435c3.png",
+                "block": "dish"
+            }
+            ,
+            {
+                "id": 1020033,
+                "store_id": 200138,
+                "class_id": 1,
+                "name": "美极木耳",
+                "name_en": " Black Fungus with Maggi Seasoning ",
+                "code": "10009",
+                "price": "23.00",
+                "member_price": "23.00",
+                "room_price": "23.00",
+                "room_member_price": "23.00",
+                "image_4": "http://static.xibeidev.com/uploads/food/0977e438658fadf21034cbe8111435c3.png",
+                "image_5": "http://static.xibeidev.com/uploads/food/2f6d9043a4e6361d44987e0bb2306ea8.jpg",
+                "image_6": "http://static.xibeidev.com/uploads/food/14db235574de012af6d68db1645509e0.png",
+                "image_7": "http://static.xibeidev.com/uploads/food/7888a22748cc3645088f54d8ff142473.png",
+                "unit": "份",
+                "unit2": "180g",
+                "attrs": [
+                    {
+                        "id": "1",
+                        "name": "辣度",
+                        "values": [
+                            {
+                                "id": "1",
+                                "name": "不辣"
+                            },
+                            {
+                                "id": "2",
+                                "name": "微辣"
+                            },
+                            {
+                                "id": "3",
+                                "name": "中辣"
+                            }
+                        ],
+                        "multiple": 0
+                    },
+                    {
+                        "id": "2",
+                        "name": "忌口",
+                        "values": [
+                            {
+                                "id": "5",
+                                "name": "免葱"
+                            },
+                            {
+                                "id": "7",
+                                "name": "免蒜"
+                            },
+                            {
+                                "id": "8",
+                                "name": "免香菜"
+                            },
+                            {
+                                "id": "6",
+                                "name": "免姜"
+                            }
+                        ],
+                        "multiple": 1
+                    }
+                ],
+                "is_new": 0,
+                "is_specialty": 0,
+                "is_essential": 0,
+                "is_recommend": 0,
+                "is_child": 0,
+                "is_weigh": 0,
+                "spicy_level": 1,
+                "is_must_spicy": 1,
+                "description": "甄选东北秋木耳，肉质肥厚，口感脆爽！主料：木耳不可免辣过敏原食物：芥末",
+                "weigh_description": "",
+                "state": 1,
+                "updatetime": 1619093569,
+                "dish_type": 1,
+                "dish_modifiers": "",
+                "dish_specs": "",
+                "sale_num": 1,
+                "image_type": "10",
+                "use_source_name": "0977e438658fadf21034cbe8111435c3.png",
+                "use_source_name2": "0977e438658fadf21034cbe8111435c3.png",
+                "block": "dish"
+            }
+            ,
+            {
+                "id": 1020033,
+                "store_id": 200138,
+                "class_id": 1,
+                "name": "美极木耳",
+                "name_en": " Black Fungus with Maggi Seasoning ",
+                "code": "10009",
+                "price": "23.00",
+                "member_price": "23.00",
+                "room_price": "23.00",
+                "room_member_price": "23.00",
+                "image_4": "http://static.xibeidev.com/uploads/food/0977e438658fadf21034cbe8111435c3.png",
+                "image_5": "http://static.xibeidev.com/uploads/food/2f6d9043a4e6361d44987e0bb2306ea8.jpg",
+                "image_6": "http://static.xibeidev.com/uploads/food/14db235574de012af6d68db1645509e0.png",
+                "image_7": "http://static.xibeidev.com/uploads/food/7888a22748cc3645088f54d8ff142473.png",
+                "unit": "份",
+                "unit2": "180g",
+                "attrs": [
+                    {
+                        "id": "1",
+                        "name": "辣度",
+                        "values": [
+                            {
+                                "id": "1",
+                                "name": "不辣"
+                            },
+                            {
+                                "id": "2",
+                                "name": "微辣"
+                            },
+                            {
+                                "id": "3",
+                                "name": "中辣"
+                            }
+                        ],
+                        "multiple": 0
+                    },
+                    {
+                        "id": "2",
+                        "name": "忌口",
+                        "values": [
+                            {
+                                "id": "5",
+                                "name": "免葱"
+                            },
+                            {
+                                "id": "7",
+                                "name": "免蒜"
+                            },
+                            {
+                                "id": "8",
+                                "name": "免香菜"
+                            },
+                            {
+                                "id": "6",
+                                "name": "免姜"
+                            }
+                        ],
+                        "multiple": 1
+                    }
+                ],
+                "is_new": 0,
+                "is_specialty": 0,
+                "is_essential": 0,
+                "is_recommend": 0,
+                "is_child": 0,
+                "is_weigh": 0,
+                "spicy_level": 1,
+                "is_must_spicy": 1,
+                "description": "甄选东北秋木耳，肉质肥厚，口感脆爽！主料：木耳不可免辣过敏原食物：芥末",
+                "weigh_description": "",
+                "state": 1,
+                "updatetime": 1619093569,
+                "dish_type": 1,
+                "dish_modifiers": "",
+                "dish_specs": "",
+                "sale_num": 1,
+                "image_type": "10",
+                "use_source_name": "0977e438658fadf21034cbe8111435c3.png",
+                "use_source_name2": "0977e438658fadf21034cbe8111435c3.png",
+                "block": "dish"
+            }
+        ]
+    }
+}
